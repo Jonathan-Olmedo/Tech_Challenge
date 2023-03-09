feaa39696a8efb02480780b2a86b1abc7319afa7
Jonathan <jolmedop04@gmail.com>
2023-03-09 17:58:16 -0600

    test

---
+++ src/arrays/arrays.test.js
@@ -0,0 +1,38 @@
+beforeEach(() => {
+  jest.resetModules();
+});
+
+test("Arreglo anidado 1", () => {
+  const { arrayIntegers } = require("./index");
+  const result = arrayIntegers([1, [2, [3, [4, 5]]]]);
+  expect(result).toStrictEqual([1,2,3,4,5]);
+});
+test("Arreglo anidado ", () => {
+  const { arrayIntegers } = require("./index");
+  const result1 = arrayIntegers([1, [2, [7,8], [3, [4, 5]], [4], [10, [13,15]]]]);
+  expect(result1).toStrictEqual([1,2,3,4,5,7,8,10,13,15]);
+});
+test("Arreglo no debe contener strings", () => {
+  const { arrayIntegers } = require("./index");
+  expect(() => arrayIntegers(["one", "two", "three"])).toThrow(Error);
+});
+test("Arreglo vacio", () => {
+  const { arrayIntegers } = require("./index");
+  expect(() => arrayIntegers([])).toThrow(Error);
+});
+test("No es un array", () => {
+  const { arrayIntegers } = require("./index");
+  expect(() => arrayIntegers(45)).toThrow(Error);
+});
+test("Arreglo de numeros y strings", () => {
+  const { arrayIntegers } = require("./index");
+  expect(() => arrayIntegers([["Hola"], [5.7, 9.8, [3.4]]])).toThrow(Error);
+});
+test("Arreglo de numeros no enteros", () => {
+  const { arrayIntegers } = require("./index");
+  expect(() => arrayIntegers([1.5, 0.15, [5.7, 9.8, [3.4]]])).toThrow(Error);
+});
+test("funcion sin argumento", () => {
+  const { arrayIntegers } = require("./index");
+  expect(() => arrayIntegers()).toThrow(Error);
+});
---
+++ src/arrays/index.js
@@ -0,0 +1,46 @@
+//Arreglo plano
+let arrayPlane = [];
+//funcion que recorre cada elemento del arreglo y verificar si tiene o no un arreglo dentro
+const arrayPlaneIntegers = (arr) => {
+  arr.forEach((e) => {
+    //si el elemento no es un arreglo hace push a un nuevo arreglo
+    if (Array.isArray(e) == false) {
+      //Si existe el elemento en el arreglo ya aplanado lo omite
+      if (!arrayPlane.includes(e)) {
+        arrayPlane.push(e);
+      }
+    }
+    //si es un arreglo vuelve a ejecutar la funcion hasta hacer push a cada elemento
+    else {
+      arrayPlaneIntegers(e);
+    }
+  });
+};
+//funcion a ejecutar inicialmente que nos muestra el resultado
+const arrayIntegers = (arr) => {
+  if (Array.isArray(arr) == true) {
+    if (arr.length == 0) {
+        throw Error('array vacio');
+    } else {
+      //agrega cada areglo anidado a uno nuevo plano
+      arrayPlaneIntegers(arr);
+      arrayPlane.forEach((e) => {
+        //verifica el tipo de elemento del arreglo plano
+        if (
+          typeof e == "string" ||
+          e == " " ||
+          Number.isInteger(e) == false ||
+          typeof e == undefined
+        ) {
+          throw Error('tipo de dato invalido');
+        }
+      });
+      return arrayPlane.sort(function(a, b){return a - b});
+    }
+  } else {
+    throw Error('no es un array'); 
+  }
+};
+module.exports = {
+  arrayIntegers: arrayIntegers,
+};
